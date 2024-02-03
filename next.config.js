ecbef9fdacad68f2d8de9b1f8bccdedd0e398612
Charles Garrett <charleslavong@gmail.com>
2024-02-03 13:52:43 -0500

    chore: add origin for CORS

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   compiler: { styledComponents: true },
   reactStrictMode: true,
+  headers: async () => [
+    {
+        source: '/:path*',
+        headers: [{
+          key: 'Access-Control-Allow-Origin',
+          value: '<origin>'
+        }]
+      }
+  ]
 };
 
 module.exports = nextConfig;
